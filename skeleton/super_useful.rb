ea2364c92ca5f9d648c05c507dc57d09f819a6a1
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-10-01 15:26:11 -0400

    Finish Error Handling

---
+++ skeleton/super_useful.rb
@@ -0,0 +1,84 @@
+class NotCoffeeError < StandardError
+end
+
+class CoffeeError < StandardError
+end
+
+class NotLongEnoughError < StandardError
+end
+
+class EmptyStringError < StandardError
+end
+
+# PHASE 2
+def convert_to_int(str)
+  begin
+    Integer(str)
+  rescue ArgumentError => e
+    puts e
+  end
+end
+
+# PHASE 3
+FRUITS = ["apple", "banana", "orange"]
+
+def reaction(maybe_fruit)
+
+  if FRUITS.include? maybe_fruit
+    puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit == "coffee"
+    raise CoffeeError
+  else
+    raise NotCoffeeError
+  end
+end
+
+def feed_me_a_fruit
+  puts "Hello, I am a friendly monster. :)"
+
+  puts "Feed me a fruit! (Enter the name of a fruit:)"
+  begin
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit)
+  rescue CoffeeError => e
+    puts "Thanks for the coffee! But I want fruit..."
+    retry
+  rescue NotCoffeeError => e
+    puts "That's not a fruit!!"
+  end
+end
+
+# PHASE 4
+class BestFriend
+  def initialize(name, yrs_known, fav_pastime)
+    @name = name
+    @yrs_known = yrs_known
+    @fav_pastime = fav_pastime
+
+    begin
+      raise NotLongEnoughError if yrs_known < 5
+    rescue NotLongEnoughError => e
+      puts "I don't know you that long, girl!"
+      exit
+    end
+
+    begin
+      raise EmptyStringError if name.length == 0 || fav_pastime.length == 0
+    rescue EmptyStringError => e
+      puts "You need a name and favorite past time!"
+      exit
+    end
+  end
+
+  def talk_about_friendship
+    puts "Wowza, we've been friends for #{@yrs_known} years. Let's be friends for another #{1000 * @yrs_known} years."
+  end
+
+  def do_friendstuff
+    puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
+  end
+
+  def give_friendship_bracelet
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me."
+  end
+end
